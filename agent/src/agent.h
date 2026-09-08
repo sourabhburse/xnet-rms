972546193cb48c46cf6122fe916539b56a09e34a
Sourabh Burse <sourabh.burse@gmail.com>
2026-09-08 17:56:20 +0530

    Bump agent for cellular payload normalization

--- agent/src/agent.h
+++ agent/src/agent.h
@@ -10,7 +10,7 @@
 #include <time.h>
 #include "parson.h"
 
-#define AGENT_VERSION "2.1.1"
+#define AGENT_VERSION "2.1.2"
 #define DEFAULT_SERVER ""
 #define DEFAULT_MQTT_HOST ""
 #define DEFAULT_MQTT_PORT 8883
